18449eaacd854c4d8b5c928a461b807b9ece15a9
Kamil <gug.kamil@gmail.com>
2026-07-24 07:53:39 +0200

    chore: build step

--- hosts/kkbook/default.nix
+++ hosts/kkbook/default.nix
@@ -66,12 +66,6 @@
     podman-compose
   ];
 
-  # SOPS TEST
-
-
-
-  # SOPS TEST END`
-
   networking.hostName = "kkbook";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
