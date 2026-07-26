1103667f61db27c1d14c26b28562813655dcea34
Kamil <gug.kamil@gmail.com>
2026-07-26 13:09:32 +0200

    chore: build step

--- system-modules/gaming.nix
+++ system-modules/gaming.nix
@@ -26,10 +26,10 @@
     fi
 
     if [ -n "$(nvidia-offload)" ]; then
-        TO_RUN="nvidia-offload \
-          __NV_PRIME_RENDER_OFFLOAD=1 \
+        TO_RUN="__NV_PRIME_RENDER_OFFLOAD=1 \
           __GLX_VENDOR_LIBRARY_NAME=nvidia \
           __VK_LAYER_NV_optimus=NVIDIA_only \
+          nvidia-offload \
           $TO_RUN"
     fi
 
