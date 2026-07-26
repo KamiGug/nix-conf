f46bfd4f7716ba42abc744d8a71957d6290c04dd
Kamil <gug.kamil@gmail.com>
2026-07-26 19:59:15 +0200

    chore: build step

---
+++ system-modules/autologin.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  cfg = config.my.autologin;
+in {
+  options.my.autologin = {
+    enable = lib.mkEnableOption "enable autologin";
+    user = lib.mkOption {
+      type = lib.types.string;
+      default = "peon";
+      description = "Select user to autologin as";
+    };
+    de = lib.mkOption {
+      type = lib.types.string;
+      default = "niri";
+      description = "Select DE to log in to";
+    };
+  };
+
+  # TODO: make this get DM from using helper
+  config = lib.mkIf cfg.enable {
+    services.displayManager.sddm = {
+        enable = true;
+
+        autoLogin = {
+          enable = true;
+          user = "${cfg.user}";
+        };
+
+        wayland = {
+          enable = true;
+        };
+      };
+
+      services.displayManager.defaultSession = "${cfg.de}";
+  };
+}
