49512b5867516264e4c5f7194370e355529f66c0
Kamil <gug.kamil@gmail.com>
2026-09-07 05:35:35 +0200

    chore: build step

--- home-modules/zed.nix
+++ home-modules/zed.nix
@@ -16,7 +16,6 @@
       yaml = pkgs.yaml-language-server;
       nix = pkgs.nixd;
       nixFormatter = pkgs.alejandra;
-      tix = pkgs.tix;
     }
     // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
       csharp = pkgs.csharp-ls;
