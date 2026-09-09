15a6b5aaa3068f9d0c0a415cdb86f121c6cc4514
Kamil <gug.kamil@gmail.com>
2026-09-09 05:13:05 +0200

    chore: build step

--- system-modules/default.nix
+++ system-modules/default.nix
@@ -1,11 +1,12 @@
-[
   # TODO: make the common files actual nixos modules
+  # TODO: similarly to home modules make this a set with common, nixos and darwin
+[
   # ./common.nix
   # ./common-linux.nix
   # ./common-gui-linux.nix
   # ./common-darwin.nix
-  # ./gaming.nix
-  # ./avahi.nix
-  # ./autologin.nix
-  # ./printing.nix
+  ./gaming.nix
+  ./avahi.nix
+  ./autologin.nix
+  ./printing.nix
 ]
